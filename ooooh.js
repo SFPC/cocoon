29aa212242dec119f80835b3fb7cedb40230af72
emma rae <doodybrainz@gmail.com>
2021-06-09 13:28:56 -0400

    Start to work on text formatting

--- ooooh.js
+++ ooooh.js
@@ -1,13 +1,2 @@
-const shell = document.getElementById('shell');
-const info = document.getElementById('info');
 
-shell.addEventListener('touchstart', function(event) {
-  info.classList.add('touched');
-}, false);
-
-shell.addEventListener('touchend', function(event) {
-  if (info.classList.contains('touched')) {
-    info.classList.remove('touched');
-  }
-}, false);
 
--- outside.js
+++ outside.js
@@ -3,21 +3,54 @@ var counter=0;
 $(window).scroll(function () {
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
         endlessScrollText();
+        activeLinks();
     }
-    console.log(counter);
-
-    // if(counter>3) {
-    //   $('.info').find('.intro-text').first().remove();
-    // }
-
 });
 
 function endlessScrollText() {
-
-    infotext = '<div class="intro-text"><p>The School for Poetic Computation is changing. This summer, we are making a temporary space together—inviting others into our (un)learning and into our collective studies. In the process, we are developing experimental curriculum that can later grow and morph to be taught again and again. Unlearning is a process of learning to unlearn with others in order to change the conditions of learning itself. We are seeking new “schooling” positions that facilitate the oscillation between “teacher" and “learner." How do we go into the cocoon, again, and how do we leave it behind?</p><p>COCOON includes sessions taught and organized by Firstname Lastname, Firstname Lastname, Firstname Lastname, Firstname Lastname, Firstname Lastname and Firstname Lastname.</p><p>The official programming schedule will be released on June 12th. Follow us on <a href="" target="_blank">Twitter</a> and <a href="" target="_blank">Instagram</a> for updates and announcements. We will be updating this web page a lot over the summer. Reach out to us <a href="">here</a>.</p><div class="peek"><a href="programs/index.html" class="peek">P e e k &nbsp; I n s i d e</a></div></div>'
-
-    $('#info').append(infotext);
+    let infoTextInner = $('#info').html();
+    $('#shell').append(infoTextInner);
     counter++;
+}
 
+function activeLinks() {
+  const shell = document.getElementById('shell');
+  const info = document.getElementById('info');
+  let expandableLinks = document.querySelectorAll('.expand');
 
+  expandableLinks.forEach((link) => {
+    link.addEventListener('click', function() {
+      console.log(link);
+      showExpanded(link);
+    })
+  })
+
+  function showExpanded(link) {
+    hideOtherExpandedLinks();
+    link.classList.add('active');
+    link.children[0].style.display = "inline";
+  }
+
+  function hideOtherExpandedLinks() {
+    expandableLinks.forEach((link) => {
+      if (link.classList.contains('active')) {
+        link.classList.remove('active');
+      }
+      
+      if (link.children.length > 0) {
+        console.log(link.children[0])
+        link.children[0].style.display = "none";
+      }
+    })
+  }
+
+  shell.addEventListener('touchstart', function() {
+    info.classList.add('touched');
+  }, false);
+
+  shell.addEventListener('touchend', function() {
+    if (info.classList.contains('touched')) {
+      info.classList.remove('touched');
+    }
+  }, false);
 }
