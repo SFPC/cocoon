93af45d79dbd39f82df7b2dae0069aa15326f578
Zainab Aliyu <zoaliyu@gmail.com>
2021-06-10 02:03:53 -0400

    update colors, bg rotate test

--- outside.js
+++ outside.js
@@ -65,3 +65,21 @@ function activeLinks() {
     }
   }, false);
 }
+
+
+$(function() {
+    var rotation = 0,
+        scrollLoc = $(document).scrollTop();
+    $(window).scroll(function() {
+        var newLoc = $(document).scrollTop();
+        var diff = scrollLoc - newLoc;
+        rotation += diff / 2, scrollLoc = newLoc;
+        // var rotationStr = "rotate(" + rotation + "deg) scale(" + rotation + ")";
+        var rotationStr = "rotate(" + rotation + "deg)";
+        $(".spiral").css({
+            "-webkit-transform": rotationStr,
+            "-moz-transform": rotationStr,
+            "transform": rotationStr
+        });
+    });
+})
