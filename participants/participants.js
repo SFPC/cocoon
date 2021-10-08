bcc7a10e0c56ab40474f37c8b0de8d2951e5b8cb
Zainab Aliyu <zoaliyu@gmail.com>
2021-10-07 22:39:05 -0400

    grid

--- participants/participants.js
+++ participants/participants.js
@@ -17,15 +17,18 @@ function addParticipantDiv(p){
   const arena = p["Are.na"] ? `<a href="${p["Are.na"]}" target="_blank">are.na</a> //` : ""
   const socials = instagram + twitter + arena
   const participantDiv = `
-  <div class="participantDiv">
+  <div class="session-info">
+    <div class="description">
+  <section>
+    <div class="participantDiv">
   <h1><a href="${p.Website}" target="_blank">${p["Your first and last name"]}</a> (${pronouns})</h1>
   <h4>// ${socials}</h4>
   <img style="width:200px" src="https://drive.google.com/uc?export=view&id=${photoId}" />
   <p>${p["Please share your bio. In 150 words or less, introduce yourself. Invitation to talk about your practice, your history, and/or some of the things you are most interested in."]}</p>
-  </div>
+</div>
+</section> </div></div>
   `
   console.log(participantDiv)
   console.log(sessionId)
   $(sessionId).append(participantDiv)
 }
-
